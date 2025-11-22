9bf95b53dbe2b2c32d129cf239185ebe77cdbc15
rkdaas <gupta.ritik2002@gmail.com>
2025-11-22 10:05:17 +0000

    commit8

--- Python-app/scripts/deploy.sh
+++ Python-app/scripts/deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Find the latest deployment-archive folder
-DEPLOY_DIR=$(ls -d /opt/codedeploy-agent/deployment-root/*/*/deployment-archive | head -1)
+ DEPLOY_DIR=$(ls -d /opt/codedeploy-agent/deployment-root/*-*-*-*-* 2>/dev/null | head -1)
+ cd "$DEPLOY_DIR"
+ DEPLOYMENT_ID=$(ls -td d-* 2>/dev/null | head -1)
+ cd "$DEPLOYMENT_ID"/deployment-archive/src/
+ cp hello.py /tmp/hello.py
+ 
 
-# Copy hello.py to /tmp
-cp "$DEPLOY_DIR/src/hello.py" /tmp/
